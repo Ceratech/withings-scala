7b7abb0f4f9e438b83b916eb872a7f22b002f5bd
Dries Schulten <dries.schulten@fastmail.com>
2017-12-06 16:07:26 +0100

    Add 'Server' as a trait to allow extending/altering it's behavior.

--- rest/src/main/scala/io/ceratech/withings/rest/Server.scala
+++ rest/src/main/scala/io/ceratech/withings/rest/Server.scala
@@ -3,12 +3,13 @@ package io.ceratech.withings.rest
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
 import io.ceratech.withings.WithingsClient
 import io.ceratech.withings.rest.swagger.SwaggerDocService
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.Properties
 
 /**
@@ -16,15 +17,15 @@ import scala.util.Properties
   *
   * @author dries
   */
-object Server extends App with LazyLogging {
+trait Server extends LazyLogging {
   // Read needed properties to boot the server
-  val port = Properties.envOrElse("PORT", "8080").toInt
-  val apiKey = Properties.envOrElse("API_KEY", "<unkown>")
-  val apiSecret = Properties.envOrElse("API_SECRET", "<unkown>")
+  val port: Int = Properties.envOrElse("PORT", "8080").toInt
+  val apiKey: String = Properties.envOrElse("API_KEY", "<unkown>")
+  val apiSecret: String = Properties.envOrElse("API_SECRET", "<unkown>")
 
   // Setup API client and REST resources
-  lazy val withingsClient = WithingsClient(apiKey, apiSecret)
-  lazy val withingsResource = new WithingsResource(withingsClient)
+  lazy val withingsClient: WithingsClient = WithingsClient(apiKey, apiSecret)
+  lazy val withingsResource: WithingsResource = new WithingsResource(withingsClient)
 
   // Initialize Akka
   implicit val system: ActorSystem = ActorSystem("withings-rest")
@@ -32,14 +33,25 @@ object Server extends App with LazyLogging {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   // Setup routes and start server
-  lazy val routes = withingsResource.routes ~ SwaggerDocService.routes
-  val bindingFuture = Http().bindAndHandle(routes, "0.0.0.0", port)
+  lazy val routes: Route = withingsResource.routes ~ SwaggerDocService.routes
 
-  logger.info(s"Server started on port $port")
+  /**
+    * Runs the server
+    */
+  def startServer(): Unit = {
+    val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "0.0.0.0", port)
 
-  // Shutdown hook to stop server on JVM shutdown
-  sys.addShutdownHook {
-    bindingFuture.flatMap(_.unbind())
-      .onComplete(_ ⇒ system.terminate())
+    logger.info(s"Server started on port $port")
+
+    // Shutdown hook to stop server on JVM shutdown
+    sys.addShutdownHook {
+      bindingFuture.flatMap(_.unbind())
+        .onComplete(_ ⇒ system.terminate())
+    }
+    ()
   }
+}
+
+object Server extends App with Server {
+  startServer()
 }
